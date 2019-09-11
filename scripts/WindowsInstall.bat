607c1bab3347ea9a35085704f70b63dfdd68a8f4
Wu Jiahao <wujiahao082@163.com>
2019-09-11 14:23:25 +0800

    update window script

--- scripts/WindowsInstall.bat
+++ scripts/WindowsInstall.bat
@@ -2,7 +2,7 @@ pip install virtualenv
 virtualenv --no-site-packages venv
 venv\Scripts\activate
 pip install -r win_requirements.txt
-pyinstaller -F -w ..\src\main.py
+pyinstaller -F -w src\main.py
 
 move dist\main.exe .
 
