818182ed0fd26646f37b3ddede8698aeaab29125
Wu Jiahao <wujiahao15@mails.ucas.ac.cn>
2020-03-16 16:44:33 +0800

    init

--- scripts/MacOsInstall.sh
+++
@@ -1,4 +0,0 @@
-pip install virtualenv
-virtualenv --no-site-packages venv
-source venv/bin/activate
-pip install -r pyrequirements/macOS.txt
--- scripts/WindowsInstall.bat
+++
@@ -1,3 +0,0 @@
-pip install virtualenv
-virtualenv --no-site-packages venv
-venv\Scripts\pip.exe install -r pyrequirements\windows10.txt
